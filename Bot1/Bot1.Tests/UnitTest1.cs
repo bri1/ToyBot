b832940380bebd936eb1f8adfccc43fe082ed1ec
Briana <briana.barker1111@gmail.com>
2019-11-24 04:13:11 +1100

    created tests for Move, Left, Right
    Unsure if switch statements are allowed. 
    Also unsure how to assert for console.log for Report command

--- Bot1/Bot1.Tests/UnitTest1.cs
+++ Bot1/Bot1.Tests/UnitTest1.cs
@@ -54,6 +54,186 @@ namespace Bot1.Tests
         }
 
 
+        [Theory]
+        [InlineDataAttribute(0, 0, "NORTH")]
+        [InlineDataAttribute(0, 0, "SOUTH")]
+        [InlineDataAttribute(0, 0, "EAST")]
+        [InlineDataAttribute(0,0, "WEST")]
+
+
+        public void Left_command_should_change_bot_direction_in_anticlockwise_direction(int xCoordinate, int yCoordinate, string direction)
+        {
+            //Arrange
+            _testBot.Place(xCoordinate, yCoordinate, direction);
+            
+
+            //Act
+            _testBot.Left();
+
+            //Assert
+            switch (direction) {
+                case "NORTH":
+                    Assert.Equal("WEST", _testBot.Direction);
+                    break;  //Can we put breaks in a test?
+                case "SOUTH":
+                    Assert.Equal("EAST", _testBot.Direction);
+
+                    break;
+
+                case "EAST":
+                    Assert.Equal("NORTH", _testBot.Direction);
+
+                    break;
+
+                case "WEST":
+                    Assert.Equal("SOUTH", _testBot.Direction);
+
+                    break;
+            }
+
+        
+        }
+
+
+
+        [Theory]
+        [InlineDataAttribute(0, 0, "NORTH")]
+        [InlineDataAttribute(0, 0, "SOUTH")]
+        [InlineDataAttribute(0, 0, "EAST")]
+        [InlineDataAttribute(0, 0, "WEST")]
+
+
+        public void Right_command_should_change_bot_direction_in_clockwise_direction(int xCoordinate, int yCoordinate, string direction)
+        {
+            //Arrange
+            _testBot.Place(xCoordinate, yCoordinate, direction);
+
+
+            //Act
+            _testBot.Right();
+
+            //Assert
+            switch (direction)
+            {
+                case "NORTH":
+                    Assert.Equal("EAST", _testBot.Direction);
+                    break;
+                case "SOUTH":
+                    Assert.Equal("WEST", _testBot.Direction);
+
+                    break;
+
+                case "EAST":
+                    Assert.Equal("SOUTH", _testBot.Direction);
+
+                    break;
+
+                case "WEST":
+                    Assert.Equal("NORTH", _testBot.Direction);
+
+                    break;
+            }
+
+
+        }
+
+        //MIGHT NEED TO RENAME THIS ONE
+        public void Move_command_should_increase_coordinate_by_one_in_facing_direction(int xCoordinate, int yCoordinate, string direction)
+        {
+            //Arrange
+            _testBot.Place(xCoordinate, yCoordinate, direction);
+
+
+            //Act
+            _testBot.Move();
+
+            //Assert
+            switch (direction)
+            {
+                case "NORTH":
+                    Assert.Equal(yCoordinate + 1, _testBot.YCoordinate);
+                    Assert.Equal(xCoordinate, _testBot.XCoordinate);
+                    Assert.Equal(direction, _testBot.Direction);
+                    break;
+                case "SOUTH":
+                    Assert.Equal(yCoordinate, _testBot.YCoordinate);
+                    Assert.Equal(xCoordinate, _testBot.XCoordinate);
+                    Assert.Equal(direction, _testBot.Direction);
+                    break;
+
+                case "EAST":
+                    Assert.Equal(yCoordinate, _testBot.YCoordinate);
+                    Assert.Equal(xCoordinate, _testBot.XCoordinate);
+                    Assert.Equal(direction, _testBot.Direction);
+                    break;
+
+                case "WEST":
+                    Assert.Equal(yCoordinate, _testBot.YCoordinate);
+                    Assert.Equal(xCoordinate, _testBot.XCoordinate);
+                    Assert.Equal(direction, _testBot.Direction);
+
+                    break;
+            }
+
+        }
+
+       
+
+        [Theory]
+        [InlineDataAttribute(0, 0, "SOUTH")]
+        [InlineDataAttribute(0, 0, "WEST")]
+        [InlineDataAttribute(0, 3, "WEST")]
+        [InlineDataAttribute(4, 4, "NORTH")]
+        [InlineDataAttribute(4, 4, "EAST")]
+        [InlineDataAttribute(1, 4, "NORTH")]
+        [InlineDataAttribute(4, 3, "EAST")]
+               
+        public void Move_command_should_do_nothing_when_bot_is_trying_to_move_off_edge_of_table(int xCoordinate, int yCoordinate, string direction)
+        {
+            //Arrange
+            _testBot.Place(xCoordinate, yCoordinate, direction);
+
+
+            //Act
+            _testBot.Move();
+
+            //Assert
+            Assert.Equal(xCoordinate, _testBot.XCoordinate);
+            Assert.Equal(yCoordinate, _testBot.YCoordinate);
+            Assert.Equal(direction, _testBot.Direction);
+                       
+        }
+
+
+
+        [Theory]
+        [InlineDataAttribute(0, 0, "SOUTH")]
+        [InlineDataAttribute(0, 0, "WEST")]
+        [InlineDataAttribute(0, 3, "WEST")]
+        [InlineDataAttribute(4, 4, "NORTH")]
+        [InlineDataAttribute(4, 4, "EAST")]
+        public void Report_command_output_correct_details(int xCoordinate, int yCoordinate, string direction)
+        {
+            //Arrange
+            _testBot.Place(xCoordinate, yCoordinate, direction);
+
+
+            //Act
+            _testBot.Report();
+
+            //Assert
+
+            //Assert.Equal( , _testBot.Report.GetOutput())
+
+
+
+        }
+
+
+
+
+
+
 
     }
 }
